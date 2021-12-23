29364911f49a00df2555cc96d8e4cbf39cb238c7
EastSunrise <wsg787@126.com>
2021-12-23 15:28:57 +0800

    Adds module commons-system to handle system-related operations. Adds the executor implementation of command line and ffmpeg as its example.

---
+++ commons-system/src/main/java/cn/wsg/commons/system/cmd/ffmpeg/SubtitleStream.java
@@ -0,0 +1,25 @@
+package cn.wsg.commons.system.cmd.ffmpeg;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+/**
+ * The subtitle stream.
+ *
+ * @author Kingen
+ */
+@Getter
+public class SubtitleStream extends AbstractStream {
+
+    @JsonProperty("width")
+    private Integer width;
+
+    @JsonProperty("height")
+    private Integer height;
+
+    @JsonProperty("tags")
+    private BaseTags tags;
+
+    SubtitleStream() {
+    }
+}
