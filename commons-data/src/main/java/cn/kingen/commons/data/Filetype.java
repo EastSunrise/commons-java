b066698ed701744ae9dc26be92026ae073495e93
Kingen <wsg787@126.com>
2023-03-12 22:26:21 +0800

    扩展文件类型

--- commons-data/src/main/java/cn/kingen/commons/data/Filetype.java
+++ commons-data/src/main/java/cn/kingen/commons/data/Filetype.java
@@ -12,33 +12,41 @@ public enum Filetype {
 
     // image types
 
-    JPG(".jpg", Type.IMAGE, "image/jpeg"),
-    JPEG(".jpeg", Type.IMAGE, "image/jpeg"),
-    JFIF(".jfif", Type.IMAGE, "image/jpeg"),
-    PNG(".png", Type.IMAGE, "image/png"),
     BMP(".bmp", Type.IMAGE, "image/bmp"),
     GIF(".gif", Type.IMAGE, "image/gif"),
+    ICO(".ico", Type.IMAGE, "image/x-icon"),
+    JPG(".jpg", Type.IMAGE, "image/jpeg"),
+    JPEG(".jpeg", Type.IMAGE, "image/jpeg"),
+    JIF(".jif", Type.IMAGE, "image/jpeg"),
+    JFIF(".jfif", Type.IMAGE, "image/jpeg"),
+    PNG(".png", Type.IMAGE, "image/png"),
     SVG(".svg", Type.IMAGE, "image/svg+xml"),
     TIF(".tif", Type.IMAGE, "image/tiff"),
     TIFF(".tiff", Type.IMAGE, "image/tiff"),
-    IEF(".ief", Type.IMAGE, "image/ief"),
+    WEBP(".webp", Type.IMAGE, "image/webp"),
 
     // video types
 
-    MP4(".mp4", Type.VIDEO, "video/mp4"),
-    M4V(".m4v", Type.VIDEO, "video/mp4"),
-    MPG(".mpg", Type.VIDEO, "video/mpeg"),
-    MPEG(".mpeg", Type.VIDEO, "video/mpeg"),
+    THREE_GP(".3gp", Type.VIDEO, "video/3gpp"),
+    AVI(".avi", Type.VIDEO, "video/x-msvideo"),
+    FLV(".flv", Type.VIDEO, "video/x-flv"),
+    H264(".h264", Type.VIDEO, "video/h264"),
+    MKV(".mkv", Type.VIDEO, "video/x-matroska"),
     MOV(".mov", Type.VIDEO, "video/quicktime"),
+    MP4(".mp4", Type.VIDEO, "video/mp4"),
+    MPEG(".mpeg", Type.VIDEO, "video/mpeg"),
+    TS(".ts", Type.VIDEO, "video/mp2t"),
+    WEBM(".webm", Type.VIDEO, "video/webm"),
+    WMV(".wmv", Type.VIDEO, "video/x-ms-wmv"),
 
     // audio types
 
-    MP3(".mp3", Type.AUDIO, "audio/mpeg"),
-    FLAC(".flac", Type.AUDIO, "audio/flac"),
     AAC(".aac", Type.AUDIO, "audio/aac"),
-    WAV(".wav", Type.AUDIO, "audio/x-wav"),
-    M4A(".m4a", Type.AUDIO, "audio/mp4"),
-    OGG(".ogg", Type.AUDIO, "audio/ogg");
+    MP3(".mp3", Type.AUDIO, "audio/mpeg"),
+    OGG(".ogg", Type.AUDIO, "audio/ogg"),
+    WAV(".wav", Type.AUDIO, "audio/wav"),
+    FLAC(".flac", Type.AUDIO, "audio/flac"),
+    ;
 
     private final String extension;
     private final Type type;
